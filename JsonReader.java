1dbc83eb18ca1fa27fb65559aa751f856980e2f8
rdgarber <67057473+rdgarber@users.noreply.github.com>
2020-07-28 21:53:34 -0400

    Add files via upload
    Upgraded class structure, created two types of reads, one for JSONObject and the other for JSONArray

--- JsonReader.java
+++ JsonReader.java
@@ -6,9 +6,11 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.*;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 
 public class JsonReader {
@@ -17,34 +19,81 @@ public class JsonReader {
     StringBuilder sb = new StringBuilder();
     int cp;
     while ((cp = rd.read()) != -1) {
-      sb.append((char) cp);
+        sb.append((char) cp);
     }
     return sb.toString();
   }
 
-  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+  public static String readtextFromUrl(String url) throws IOException, JSONException {
+	JSONObject json;
     InputStream is = new URL(url).openStream();
     try {
+	  JSONArray ja;
       BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-      String jsonText = readAll(rd);
-      JSONObject json = new JSONObject(jsonText);
-      return json;
+	  String jsonText = readAll(rd);
+	  return jsonText;
     } finally {
-      is.close();
+      is.close();               
     }
   }
+  
+	public static JSONObject getJsonObj(String url) throws IOException, JSONException  {
+		String jsonText = readtextFromUrl(url);
+		JSONObject json;
+		
+		if (jsonText.charAt(0) == '[')	{  // we have a JSON array here
+			System.out.println("Error - You tried to get an object and istead found an array");
+			return null;
+		  }  
+		 // we just have a plain JSON Object
+		 json = new JSONObject(jsonText);
+		 return json;
+	}
+
+	public static JSONArray getJsonArray(String url) throws IOException, JSONException  {
+		JSONArray ja;
+		String jsonText = readtextFromUrl(url);
+		
+		if (jsonText.charAt(0) != '[')	{  // we have a JSON array here
+			System.out.println("Error - You tried to get an array but there is no array here");
+			return null;
+		  }  
+		 // we just have a plain JSON Object
+		 ja = new JSONArray(jsonText);
+		 return ja;
+	}
+	
+		
+	public static Object getValue(JSONObject json, String key)	{
+		return(json.get(key));
+	}
+
+	public static ArrayList<String> getKeyList(JSONObject json)	{
+		ArrayList<String> al = new ArrayList<String>();
+		json.keys().forEachRemaining(key -> {
+			al.add(key);
+		});
+		return al;
+	}	
+	  
+	  
     public static void main(String[] args) throws IOException, JSONException {
-    JSONObject json = readJsonFromUrl("https://covidtracking.com/api/v1/states/ny/current.json");
+    //JSONObject json = getJsonObj("https://covidtracking.com/api/v1/states/ny/current.json");
+	JSONArray al = getJsonArray("https://covidtracking.com/api/v1/states/ny/daily.json");
+	JSONObject json = al.getJSONObject(0);
     System.out.println(json.toString());
 	handleJSONObject(json);
 	System.out.printf("positiveCasesViral:%s\n",json.get("positiveCasesViral"));
 	}
 	
 	public static void handleJSONObject(JSONObject jsonObject) {
-		jsonObject.keys().forEachRemaining(key -> {
+		/* jsonObject.keys().forEachRemaining(key -> {
 			Object value = jsonObject.get(key);
-			System.out.printf("Key: %s\tValue: %s\n", key, value);
-		});	
+		*/
+			ArrayList<String> al = getKeyList(jsonObject);
+			for (int i=0; i<al.size(); i++)
+				System.out.printf("Key: %s\tValue: %s\n", al.get(i), getValue(jsonObject, al.get(i)).toString());
+			
     }
 
 }
